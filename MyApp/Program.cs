32b99e2ae44d8d38c4f44d2568d0aa1c6b4e0016
Anton Kisliakou <s30197@pjwstk.edu.pl>
2026-03-15 18:01:54 +0100

    Add welcome message

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Statistics App!");
+Console.WriteLine("Enter 5 numbers:");
