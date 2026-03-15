49dae329385b1d968c1e3c28a3d8abf49d5e02ae
Anton Kisliakou <s30197@pjwstk.edu.pl>
2026-03-15 18:20:15 +0100

    Add sum calculation

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,19 @@
 ﻿Console.WriteLine("Welcome to Statistics App!");
 Console.WriteLine("Enter 5 numbers:");
+
+int[] numbers = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Number " + (i + 1) + ": ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum = sum + numbers[i];
+}
+
+Console.WriteLine("Sum = " + sum);
